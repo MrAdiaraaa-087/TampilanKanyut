5b4f8d7d927b461b9d0527fb49a11c3f3ba49d2c
MrAdiaraaa-087 <87253374+MrAdiaraaa-087@users.noreply.github.com>
2021-07-12 19:56:42 +0700

    Add files via upload

---
+++ TampilanKanyut.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+clear
+figlet MR Adiaraaa | lolcat
+echo
+echo "===============================================================" | lolcat
+echo "[•] Author : MR Adiaraaa                                    [•]" | lolcat
+echo "[•] Team   : Cyber Attacker                                 [•]" | lolcat
+echo "[•] WA     : 6281546772792                                  [•]" | lolcat
+echo "====================( WELCOME TO TERMINAL )====================" | lolcat
